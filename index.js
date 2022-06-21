c4a528198a17167593f20e2976c5d8a0a78ad02f
imsaifun <imsaifun@gmail.com>
2022-06-21 14:12:02 +0600

    initial

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-// Import packages
 const express = require("express");
-const home = require("./routes/home");
-
-// Middlewares
 const app = express();
+
+const path = require("path");
+const logger = require("morgan");
+const cors = require("cors");
+
+app.use(logger("dev"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
-// Routes
-app.use("/home", home);
+app.get("/api/test", (req, res) => {
+  res.send("test");
+});
 
-// connection
-const port = process.env.PORT || 9001;
-app.listen(port, () => console.log(`Listening to port ${port}`));
+app.use(express.static(path.join(__dirname, "./frontend/build")));
+
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./frontend/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server Running on port ${port}`));
+
+module.exports = app;
